2e6899a9eeb593fe79dd5b598c77001c9d5fc424
arlaspresident <jankovic4901@gmail.com>
2025-04-15 11:34:17 +0200

    lagt till get route och 404 hanterare för hade världens problem med det

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ app.get('/', (req, res) => {
     res.send('välkommen till mitt cv api');
 });
 
+app.get('/api/workexperience', (req, res) => {
+    console.log('GET /api/workexperience anropad');
+    const sql = 'SELECT * FROM workexperience';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('fel vid hämtning:', err);
+            res.status(500).json({ error: 'något gick fel vid hämtning av data' });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`servern kör på http://localhost:${port}`);
+});
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
 });
